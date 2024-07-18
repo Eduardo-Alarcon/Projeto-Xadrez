246cbc595b4ad6cf2ea7a2128a4ae913a44b5cf4
Eduardo Alarcon <eduardoalarcon111@gmail.com>
2024-07-18 14:55:09 -0300

    Criando a calsse PosiçãoXadrez

---
+++ Xadrez-Console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+namespace xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char coluna;
+        public int linha;
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a'); 
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
